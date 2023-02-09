b48de569d6cce200b59a147b14cf8f2b9a6e310c
jelanmathewjames <jelanmathewjames1234@gmail.com>
2023-02-09 12:54:29 -0600

    completed admin login and viewing registration details

--- admin/urls.py
+++ admin/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    
+    path('',views.admin_login,name="admin_login"),
+    path('admin_home',views.admin_home,name="admin_home")
 ]
