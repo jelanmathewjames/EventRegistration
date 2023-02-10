4227db15ad70a6ff29f1ad8fe5a422a878ec5a30
jelanmathewjames <72068016+jelanmathewjames@users.noreply.github.com>
2023-02-10 11:28:52 +0530

    updating url

--- admin/urls.py
+++ admin/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.admin_login,name="admin_login"),
     path('admin_home',views.admin_home,name="admin_home"),
-    path('admin_logout',views.admin_logout,name="admin_logout")
+    path('admin_logout',views.admin_logout,name="admin_logout"), 
+    path('admin_reschedule',views.admin_reschedule,name="admin_reschedule") 
 ]
